7f7862f53fa1e5647f1b93675c74b9a13c72d4ae
paque <paque.melanie@gmail.com>
2020-05-26 17:03:59 +0200

    add config component HeaderTextSwitchDown

---
+++ dom-structure/src/config/components/HeaderTextSwitchDown.js
@@ -0,0 +1,126 @@
+import {
+    settingsCTA,
+    settingsSingleImage,
+    settingsTagline,
+    settingsTemplate,
+    settingsText,
+    settingsTitle
+} from "../../utils/organism.model.config";
+
+export default {
+    model: 'HeaderTextSwitchDown',
+    order: ['Title', 'Tagline', 'Content', 'Image'],
+    fields: [
+        {
+            name: 'Template',
+            nameProperty: 'Template',
+            typeField: 'Template',
+            content: {
+                responsive: ['M', 'T', 'D'],
+                parameters: {},
+                defaultValue: {
+                    images: [
+                        {
+                            alt: {},
+                            asset: {
+                                M: {},
+                                T: {},
+                                D: {}
+                            }
+                        }
+                    ]
+                }
+            },
+            settings: settingsTemplate
+        },
+        {
+            name: 'Title',
+            nameProperty: 'Title',
+            typeField: 'Text',
+            content: {
+                responsive: ['A'],
+                parameters: {},
+                defaultValue: {
+                    text: {}
+                }
+            },
+            settings: settingsTitle
+        },
+        {
+            name: 'Tagline',
+            nameProperty: 'Tagline',
+            typeField: 'Text',
+            content: {
+                responsive: ['A'],
+                parameters: {},
+                defaultValue: {
+                    text: {}
+                }
+            },
+            settings: settingsTagline
+        },
+        {
+            name: 'Content',
+            nameProperty: 'Content',
+            typeField: 'TextMarkdown',
+            content: {
+                responsive: ['A'],
+                parameters: {},
+                defaultValue: {
+                    html: {}
+                }
+            },
+            settings: settingsText
+        },
+        {
+            name: 'Image',
+            nameProperty: 'Image',
+            typeField: 'SingleImage',
+            content: {
+                responsive: ['A'],
+                parameters: {},
+                defaultValue: {
+                    images: [
+                        {
+                            alt: {},
+                            asset: {
+                                A: {}
+                            }
+                        }
+                    ]
+                }
+            },
+            settings: settingsSingleImage
+        },
+        {
+            name: 'CTA',
+            nameProperty: 'CTA',
+            typeField: 'CTA',
+            content: {
+                responsive: ['A'],
+                parameters: {},
+                defaultValue: {
+                    text: {},
+                    link: {},
+                    icon: {}
+                }
+            },
+            settings: settingsCTA
+        },
+        {
+            name: 'CTA 2',
+            nameProperty: 'CTA2',
+            typeField: 'CTA',
+            content: {
+                responsive: ['A'],
+                parameters: {},
+                defaultValue: {
+                    text: {},
+                    link: {},
+                    icon: {}
+                }
+            },
+            settings: settingsCTA
+        }
+    ]
+};
