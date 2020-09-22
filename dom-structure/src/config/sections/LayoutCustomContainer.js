bccebec07e6c59cc3e48a03b62165a0615a0587b
paque <paque.melanie@gmail.com>
2020-09-22 17:55:42 +0200

    add LayoutCustomContainer

---
+++ dom-structure/src/config/sections/LayoutCustomContainer.js
@@ -0,0 +1,42 @@
+import {settingsFlexContainer, settingsTemplate} from "../../utils/organism.model.config";
+export default {
+    model: 'LayoutCustomContainer',
+    fields: [
+        {
+            name: 'Template',
+            nameProperty: 'Template',
+            typeField: 'Template',
+            content: {
+                responsive: ['M', 'T', 'D'],
+                parameters: {},
+                defaultValue: {
+                    images: [
+                        {
+                            alt: {},
+                            asset: {
+                                M: {},
+                                T: {},
+                                D: {}
+                            }
+                        }
+                    ]
+                }
+            },
+            settings: settingsTemplate
+
+        },
+        {
+            name: 'Flex Container',
+            nameProperty: 'FlexContainer',
+            typeField: 'FlexContainer',
+            content: {
+                responsive: ['A'],
+                parameters: {},
+                defaultValue: {
+                }
+            },
+            settings: settingsFlexContainer
+
+        }
+    ]
+};
